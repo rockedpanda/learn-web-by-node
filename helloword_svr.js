76c9779cea4710f8c676a9de40096e3c0ec4d6c6
rockedpanda <chenghuawei@gmail.com>
2019-06-11 17:06:35 +0800

    增加readme

--- helloword_svr.js
+++ helloword_svr.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    console.log(req);
+    console.log(res);
     res.end('hello world');
 });
 /* server.on('clientError', (err, socket) => {
---
+++ readme.md
@@ -0,0 +1,12 @@
+# NodeJS服务开发
+
+## 样例1: 端口转发
+转发TCP数据包到远程服务器
+
+## 样例2: hello word HTTP服务
+- 返回Hello World的服务
+- 查看请求对象
+- 查看返回对象
+- 文档: https://npm.taobao.org/mirrors/node/latest/docs/api/http.html
+
+## 样例3: 静态资源网站
