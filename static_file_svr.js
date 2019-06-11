5d0c198fea0c3b786a52ec27a359cf77844f1a0d
rockedpanda <chenghuawei@gmail.com>
2019-06-11 17:23:00 +0800

    实现基本静态文件服务

---
+++ static_file_svr.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+let BASE_DIR = './public/';
+
+function readFile(url){//演示代码,暂时使用同步判定.
+    let localPath = path.join(BASE_DIR, url);
+    if(!fs.existsSync(localPath)){
+        return false;
+    }
+    let stat = fs.statSync(localPath);
+    if(stat.isFile()){
+        return fs.createReadStream(localPath);
+    }
+    return false;
+}
+
+const server = http.createServer((req, res) => {
+    let fileStream = readFile(req.url.substring(1));
+    if(!fileStream){
+        res.statusCode = 404;
+        res.end('File Not Fount');
+        return;
+    }
+    res.statusCode = 200;
+    fileStream.pipe(res);
+    //另外一种方式是读取文件内容到内存,然后res.send(内存内容);无法处理大文件,不使用.
+});
+/* server.on('clientError', (err, socket) => {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+}); */
+server.listen(8000);
